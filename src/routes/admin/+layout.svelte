227a12d271e84b28d89d79ebebb40efc543bd53a
Martin Diaz <midiazgt@gmail.com>
2025-05-07 16:00:49 -0300

    admin layout

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import type { LayoutData } from './$types';
+
+    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+    
+    let adminMenu = [
+
+    {
+            label: 'Reservas',
+            href: '/admin'
+        },
+        {
+            label: 'Categorías',
+            href: '/admin/categorias'
+        },
+        {
+            label: 'Modelos',
+            href: '/admin/modelos'
+        },
+        {
+            label: 'Vehiculos',
+            href: '/admin/vehiculos'
+        },
+        {
+            label: 'Clientes',
+            href: '/admin/clientes'
+        },
+    ]
+</script>
+
+<div class="flex min-h-[100dvh]">
+    <nav class="flex flex-col w-96 items-end gap-6  p-4 md:px-8 sticky top-0 bg-base-200 border-r border-base-300">
+        <a href="/admin" class="text-2xl font-bold">ALQUILANDO</a>
+
+        <ul class="flex flex-col gap-2  w-full flex-1 items-start">
+            {#each adminMenu as item}
+                <li class="w-full">
+                    <a href={item.href} class="btn text-lg font-medium btn-ghost hover:border-none w-full !text-right justify-end">{item.label}</a>
+                </li>
+            {/each}
+        </ul>
+
+        <p class="btn w-full">Cerrar sesión</p>
+    </nav>
+
+    <main class="flex-1 p-8">
+        {@render children()}
+    </main>
+</div>
+
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -4,17 +4,8 @@
     let { data }: { data: PageData } = $props();
 </script>
 
-<div class="flex flex-col min-h-[100dvh]">
-    <nav class="flex justify-between items-center p-4 md:px-8">
-        <span class="text-2xl font-bold">ALQUILANDO</span>
-       
-        
-    </nav>
 
-    <main class="container flex-1 mx-auto p-4 md:px-8 flex flex-col  justify-center -mt-32">
         <p>user:</p>
         {#each Object.entries(data.user) as [key, value]}
             <p class="text-left">{key}: {value}</p>
         {/each}
-    </main>
-</div>
