83ce5e4e74167b133f47538474842970aad02ea8
Martin Diaz <midiazgt@gmail.com>
2025-05-07 15:46:51 -0300

    add user check

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,17 @@
 import type { Handle } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth.js';
-
+import { redirect } from '@sveltejs/kit';
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
 
 	if (!sessionToken) {
 		event.locals.user = null;
 		event.locals.session = null;
+
+		if (event.url.pathname.startsWith('/admin')) {
+			redirect(302, '/registrarse');
+		}
+
 		return resolve(event);
 	}
 
@@ -16,6 +21,9 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
 	} else {
 		auth.deleteSessionTokenCookie(event);
+		if (event.url.pathname.startsWith('/admin')) {
+			redirect(302, '/registrarse');
+		}
 	}
 
 	event.locals.user = user;
