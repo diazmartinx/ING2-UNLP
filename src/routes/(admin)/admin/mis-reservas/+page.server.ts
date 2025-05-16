80b9ca0b40a388da86a011860dc30f56ddc3fa12
MateoMeske <mateomeske@gmail.com>
2025-05-16 11:58:24 -0300

    muevo Mis reservas a la nueva ruta

--- src/routes/(admin)/admin/mis-reservas/+page.server.ts
+++ src/routes/(admin)/admin/mis-reservas/+page.server.ts
@@ -1,5 +1,69 @@
-import type { PageServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { error, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
+import { reservas, unidadesVehiculos, modelosVehiculos, usuarios, politicasCancelacion } from '$lib/server/db/schema';
+import { eq, desc, sql } from 'drizzle-orm';
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+export const load = (async ({ locals }) => {
+    const session = locals.session;
+    if (!session) {
+        throw redirect(303, '/');
+    }
+
+    // Obtener el usuario y su rol
+    const usuario = await db
+        .select()
+        .from(usuarios)
+        .where(eq(usuarios.id, session.userId))
+        .limit(1);
+
+    if (!usuario.length || usuario[0].rol !== 'cliente') {
+        throw error(403, 'No tienes permiso para acceder a esta página');
+    }
+
+    const reservasUsuario = await db
+        .select({
+            id: reservas.id,
+            patente: reservas.patenteUnidadAsignada,
+            fechaInicio: reservas.fechaInicio,
+            fechaFin: reservas.fechaFin,
+            estado: reservas.estado,
+            importeTotal: reservas.importeTotal,
+            marca: modelosVehiculos.marca,
+            modelo: modelosVehiculos.modelo, 
+            tipoPolitica: politicasCancelacion.tipoPolitica,
+            porcentajeReembolsoParcial: modelosVehiculos.porcentajeReembolsoParcial,
+        })
+        .from(reservas)
+        .leftJoin(unidadesVehiculos, eq(reservas.patenteUnidadAsignada, unidadesVehiculos.patente))
+        .leftJoin(modelosVehiculos, eq(unidadesVehiculos.idModelo, modelosVehiculos.id))
+        .leftJoin(politicasCancelacion, eq(modelosVehiculos.idPoliticaCancelacion, politicasCancelacion.id))
+        .where(eq(reservas.idUsuario, session.userId))
+        .orderBy(
+            desc(sql`${reservas.estado} = 'Pendiente'`),
+            (reservas.fechaInicio)
+            
+        );
+
+    return {
+        reservas: reservasUsuario
+    };
+}) satisfies PageServerLoad; 
+
+export const actions: Actions = {
+    cancelarReserva: async ({ request }) => {
+        const data = await request.formData();
+        const idReserva = Number(data.get('idReserva'));
+
+        try {
+            await db.update(reservas)
+                .set({ estado: 'Cancelada' })
+                .where(eq(reservas.id, idReserva));
+        } catch (error) {
+            console.error('Error al cancelar la reserva:', error);
+            return { success: false, error: (error as any).message };
+        }
+
+        return { success: true };
+    }
+};
