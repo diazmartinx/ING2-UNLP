f6452ab8b7c1b56f464a07bd232a629e2577be03
c97Ernesto <ernesto-cafre@outlook.com>
2025-06-01 00:39:20 -0300

    editar datos del empleado, test

--- src/routes/(admin)/admin/clientes/[id]/+page.svelte
+++ src/routes/(admin)/admin/clientes/[id]/+page.svelte
@@ -46,7 +46,9 @@
 		<div class="alert alert-error mt-2">{mensajeError}</div>
 	{/if}
 
-	<form method="POST" action="?/guardar"
+	<form
+		method="POST"
+		action="?/guardar"
 		use:enhance={() => {
 			return async ({ result, update }) => {
 				// Invalidar datos si la operación fue exitosa
@@ -54,13 +56,19 @@
 					await invalidateAll();
 					// Desactivar modo edición después de guardar exitosamente
 					isEditing = false;
-					mensajeExito = typeof result.data?.message === 'string' ? result.data.message : 'Cliente eliminado exitosamente';
+					mensajeExito =
+						typeof result.data?.message === 'string'
+							? result.data.message
+							: 'Cliente eliminado exitosamente';
 					setTimeout(() => {
 						mensajeExito = '';
 					}, 3000);
 				}
 				if (result.type === 'failure') {
-					mensajeError = typeof result.data?.error === 'string' ? result.data.error : 'Error al eliminar el cliente';
+					mensajeError =
+						typeof result.data?.error === 'string'
+							? result.data.error
+							: 'Error al eliminar el cliente';
 					setTimeout(() => {
 						mensajeError = '';
 					}, 3000);
@@ -76,41 +84,43 @@
 			};
 		}}
 	>
-		<fieldset class="fieldset">
-			<legend class="fieldset-legend">Nombre (*)</legend>
-			<input type="text" class="input validator" name="nombre" bind:value={nombre} disabled={!isEditing} required />
+		<div class="flex w-full">
+			<div class="grid grow">
+				<fieldset class="fieldset">
+					<legend class="fieldset-legend">Nombre (*)</legend>
+                    
+					<input type="text" class="input validator" name="nombre" bind:value={nombre} disabled={!isEditing} required />
 
-			<legend class="fieldset-legend">Apellido (*)</legend>
-			<input type="text" class="input validator" name="apellido" bind:value={apellido} disabled={!isEditing} required />
+					<legend class="fieldset-legend">Apellido (*)</legend>
+					<input type="text" class="input validator" name="apellido" bind:value={apellido} disabled={!isEditing} required />
 
-			<legend class="fieldset-legend">DNI (*)</legend>
-			<input type="text" class="input validator" name="dni" bind:value={dni} disabled required />
+					<legend class="fieldset-legend">DNI (*)</legend>
+					<input type="text" class="input validator" name="dni" bind:value={dni} disabled required />
 
-			<legend class="fieldset-legend">Email (*)</legend>
-			<input type="email" class="input validator" name="email" bind:value={email} disabled={!isEditing} required />
+					<legend class="fieldset-legend">Email (*)</legend>
+					<input type="email" class="input validator" name="email" bind:value={email} disabled={!isEditing} required />
 
-			<legend class="fieldset-legend">Teléfono</legend>
-			<input type="number" class="input validator" name="telefono" bind:value={telefono} disabled={!isEditing} />
+					<legend class="fieldset-legend">Teléfono</legend>
+					<input type="number" class="input validator" name="telefono" bind:value={telefono} disabled={!isEditing} />
 
-            <legend class="fieldset-legend">Fecha de Nacimiento (*)</legend>
-            <input type="date" class="input" name="fechaNacimiento" bind:value={fechaNacimiento} disabled={!isEditing} required/>
-		</fieldset>
+					<legend class="fieldset-legend">Fecha de Nacimiento (*)</legend>
+					<input type="date" class="input" name="fechaNacimiento" bind:value={fechaNacimiento} disabled={!isEditing} required />
+				</fieldset>
+			</div>
 
-		<div class="mt-2 flex gap-2">
-			{#if !isEditing}
-				<!-- Botón Editar cuando no está en modo edición -->
-				<button type="button" class="btn btn-secondary" onclick={enableEditing}>
-					Editar
-				</button>
-			{:else}
-				<!-- Botones Guardar y Cancelar cuando está en modo edición -->
-				<button type="submit" class="btn btn-primary">
-					Guardar
-				</button>
-				<button type="button" class="btn btn-outline" onclick={cancelEditing}>
-					Cancelar
-				</button>
-			{/if}
+			<div class="grid grow place-items-center justify-items-start">
+				{#if !isEditing}
+					<!-- Botón Editar cuando no está en modo edición -->
+					<button type="button" class="btn btn-soft btn-secondary" onclick={enableEditing}> Actualizar Datos </button>
+				{:else}
+					<div class="flex w-full flex-col">
+						<div class="grid place-items-center mb-1 justify-items-start"><button type="submit" class="btn btn-soft btn-primary"> Guardar </button></div>
+
+						<div class="grid place-items-center mt-1 justify-items-start"><button type="button" class="btn btn-soft " onclick={cancelEditing}> Cancelar </button></div>
+					</div>
+					<!-- Botones Guardar y Cancelar cuando está en modo edición -->
+				{/if}
+			</div>
 		</div>
 	</form>
 </div>
