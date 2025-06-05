513e022bd4c28330f3d5647124b799193c42e3c3
Franco Villarreal <franco.villarreal@hotmail.com>
2025-06-05 07:10:08 -0300

    empezando HU, haciendo que la reserva tenga idSucursal

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -53,6 +53,7 @@ export const reservas = sqliteTable('reservas', {
 	id: integer().primaryKey({ autoIncrement: true }),
 	idUsuario: integer().notNull().references(() => usuarios.id),
 	idModeloReservado: integer().notNull().references(() => modelosVehiculos.id),
+	idSucursal: integer().notNull().references(() => sucursales.id),
 	patenteUnidadAsignada: text().references(() => unidadesVehiculos.patente),
 	fechaInicio: integer({ mode: 'timestamp' }).notNull(),
 	fechaFin: integer({ mode: 'timestamp' }).notNull(),
