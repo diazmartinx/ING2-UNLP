1b29b121920294c8f4ca31e5e835e38a844cd8f6
Franco Villarreal <franco.villarreal@hotmail.com>
2025-06-05 09:34:02 -0300

    sacando las rutas de reservas-historicas que no son necesarias

--- src/routes/(admin)/admin/reservas-historicas/[id]/+page.svelte
+++ src/routes/(admin)/admin/reservas-historicas/[id]/+page.svelte
@@ -74,11 +74,11 @@
 
 <div class="container mx-auto px-4 py-8">
     <div class="mb-6">
-        <a href="/admin/reservas" class="text-blue-600 hover:text-blue-800 flex items-center">
+        <a href="/admin/reservas-historicas" class="text-blue-600 hover:text-blue-800 flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
             </svg>
-            Volver a Reservas
+            Volver a Historial de Reservas
         </a>
     </div>
 
