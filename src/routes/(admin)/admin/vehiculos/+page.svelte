3cde5f8be940c8519466ecbd6f5a838e4ff78cec
Franco Villarreal <franco.villarreal@hotmail.com>
2025-06-26 07:57:30 -0300

    añadiendo aria-label para que no se queje el código

--- src/routes/(admin)/admin/vehiculos/+page.svelte
+++ src/routes/(admin)/admin/vehiculos/+page.svelte
@@ -135,6 +135,7 @@
 			<button
 				onclick={() => successMessage = ''}
 				class="flex-shrink-0 text-green-400 hover:text-green-600 transition-colors"
+				aria-label="Cerrar notificación"
 			>
 				<svg class="h-4 w-4" fill="currentColor" viewBox="0 0 20 20">
 					<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
