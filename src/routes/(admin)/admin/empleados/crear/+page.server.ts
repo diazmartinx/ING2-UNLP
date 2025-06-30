9d6abc9d94edef938ea4d8f1d973281ea0bb4266
Martin Diaz <midiazgt@gmail.com>
2025-06-30 12:19:17 -0300

    fix contraseña empleado

--- src/routes/(admin)/admin/empleados/crear/+page.server.ts
+++ src/routes/(admin)/admin/empleados/crear/+page.server.ts
@@ -4,6 +4,7 @@ import { db } from '$lib/server/db';
 import { sendNewUserEmail } from '$lib/server/resend';
 import { usuarios } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
+import { hash } from '@node-rs/argon2';
 
 export const load = (async () => {
     return {};
@@ -30,10 +31,15 @@ export const actions: Actions = {
 		// generar contraseña de 6 caracteres y enviar por correo
 		const password = Math.random().toString(36).substring(2, 8);
 		await sendNewUserEmail(email, nombre, password);
+		console.log('Email enviado:', email);
+		console.log('Contraseña enviada por correo:', password);
+
+		// hashear contraseña
+		const passwordHash = await hash(password);
 
 		// crear empleado
 		const crearEmpleado = await db.insert(usuarios).values({
-			nombre, apellido, dni, email, passwordHash: password, rol: 'empleado' })
+			nombre, apellido, dni, email, passwordHash, rol: 'empleado' })
 			
 		if (!crearEmpleado || crearEmpleado.rowsAffected === 0) {
 			return fail(400, { error: 'Error al crear el empleado' });
